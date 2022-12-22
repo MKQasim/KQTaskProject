1ae56e4199fe621a2a184092dc5a6e89faa68c5a
KamsQue <kamqasim1@gmail.com>
2022-12-22 12:33:43 +0400

    [Muhammad] Github Users List APi Implimented at Home Screen

---
+++ KQTaskProject/App Models/HomeModel/UserModel.swift
@@ -0,0 +1,57 @@
+//
+//  HomeModel.swift
+//  KQTaskProject
+//
+//  Created by KamsQue on 21/12/2022.
+//
+
+import Foundation
+
+typealias Users = [User]
+
+// MARK: - User
+public struct User: Codable {
+    var login: String?
+    var id: Int?
+    var nodeID: String?
+    var avatarURL: String?
+    var gravatarID: String?
+    var url, htmlURL, followersURL: String?
+    var followingURL, gistsURL, starredURL: String?
+    var subscriptionsURL, organizationsURL, reposURL: String?
+    var eventsURL: String?
+    var receivedEventsURL: String?
+    var type: TypeEnum?
+    var siteAdmin: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case login, id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+    }
+}
+
+// TypeEnum.swift
+
+import Foundation
+
+enum TypeEnum: String, Codable {
+    case organization = "Organization"
+    case user = "User"
+}
+
+
--- KQTaskProject/AppCommon/UIEliments/TableViewCells/ContainerView/ContainerView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ContainerView.swift
-//  KQTaskProject
-//
-//  Created by KamsQue on 22/12/2022.
-//
-
-import Foundation
